641a476dc5252b0c88cdf39b773d1f1ce8cb7e76
Megan Lee <wylee14@gmail.com>
2018-06-08 11:54:10 +1000

    Created single ticket routes

--- main.rb
+++ main.rb
@@ -13,5 +13,15 @@ get "/" do
 end
 
 get "/ticket/:id" do
+  url = "https://meganlee18.zendesk.com/api/v2/tickets/#{params[:id]}.json"
+  result = HTTParty.get(url, :basic_auth => {username: "wylee14@gmail.com", password: "Meganlee123"})
+  all_tickets = result.parsed_response
+
+  @tickets = all_tickets["ticket"]
+  @ticket_id = all_tickets["ticket"]["id"]
+  @date_created = all_tickets["ticket"]["created_at"]
+  @requester_id = all_tickets["ticket"]["requester_id"]
+  @ticket_description = all_tickets["ticket"]["description"]
+
   erb :ticket
 end
---
+++ readme.md
@@ -0,0 +1,7 @@
+# Ticket Viewer
+View my app here: -url link to be provided after deployment-
+
+## About the program
+
+Uses Sinatra framework
+Runs on localhost: 4567
