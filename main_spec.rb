26f369704ecbaf1e53813da5f7bc3045058ba68e
Megan Lee <wylee14@gmail.com>
2018-06-13 18:56:52 +1000

    more example of test cases

--- main_spec.rb
+++ main_spec.rb
@@ -10,7 +10,7 @@ def app
   Sinatra::Application
 end
 
-describe "ticket viewer" do
+describe "index" do
   #methods that allow the use of "get '/' "
   include Rack::Test::Methods
 
@@ -19,28 +19,53 @@ describe "ticket viewer" do
     last_response.status.should == 200
     #last_response.should be_ok
   end
+
+  it "should return tickets" do
+    get "/"
+    #check api response_body
+  end
+
+  it "should change page numbers" do
+    get "/?page=2"
+    last_response.status.should == 200
+  end
+
+  it "should return tickets for second page" do
+    get "/?page=2"
+    #return tickets
+  end
 end
 
-describe "ticket viewer" do
+describe "individual ticket details" do
+  include Rack::Test::Methods
+
+  it "should load the ticket page" do
+    get "/ticket/1"
+    last_response.status.should == 200
+  end
+
+  it "should return ticket details" do
+    get "/ticket/1"
+    #check api response_body
+  end
+end
+
+describe "num_of_pages" do
   it "should return rounded up page number based on total ticket count" do
     num_of_pages(101).should == 5
   end
 end
 
-describe "calling api error" do
+describe "when zendesk api return error" do
   it "raises" do
-    expect { raise "Error calling API" }.to raise_error(RuntimeError, "Error calling API")
+    expect { call_api("http://wrong-url.com") }.to raise_error(RuntimeError, "Oh no! There is an error, try again later.")
   end
 end
 
-describe "calling a missing method" do
+describe "when api is down" do
   it "raises" do
-    expect { raise "Oh no! There is an error, try again later." }.to raise_error(RuntimeError, "Oh no! There is an error, try again later.")
+    expect { raise "Oh no! There is an error, try again later." }.to raise_error(RuntimeError, "Error calling API")
   end
 end
 
-describe "calling a runtime error" do
-  it "raises" do
-    expect { raise "Sorry, we are unable to process the request now. Please try again later." }.to raise_error(RuntimeError, "Sorry, we are unable to process the request now. Please try again later.")
-  end
-end
+#how to stub HTTParty in RSPEC
